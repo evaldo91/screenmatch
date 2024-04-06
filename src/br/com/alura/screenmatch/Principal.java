b69b397263153da733572362b539c18de565de62
evaldo91 <evaldo.but@live.com>
2024-04-06 16:37:48 -0300

    Arrumando e Finalizando tema 3 e iniciando tema 4

--- src/br/com/alura/screenmatch/Principal.java
+++ src/br/com/alura/screenmatch/Principal.java
@@ -1,3 +1,5 @@
+package br.com.alura.screenmatch;
+
 import br.com.alura.screenmatch.calculos.CalculadoraDeTempo;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
